73d5819773a5f4a6a5cdeacfa038430bc2309e1e
ko22441111 <dunovicana@gmail.com>
2024-12-13 13:15:14 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,3 @@
-
 // Import the necessary functions from Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, deleteDoc, doc, getDocs } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
@@ -23,7 +22,7 @@ async function sendMessage(username, message) {
   if (username.trim() && message.trim()) {
     // Automatically prepend [OWNER] to "Matej22441"
     if (username === "Matej22441") {
-      username = [OWNER] ${username};
+      username = `[OWNER] ${username}`;
     }
 
     // Check if the message is "/clearchat"
@@ -75,16 +74,14 @@ function listenToMessages() {
       // Check if the username is [OWNER]
       if (username.startsWith("[OWNER]")) {
         usernameSpan.classList.add("owner"); // Add the special class for [OWNER]
-        usernameSpan.style.color = "darkpink"; // Ensure OWNER is dark pink
       }
 
       // Check if the username is "Ana Dunovic"
       if (username === "Ana Dunovic") {
         usernameSpan.classList.add("owners-girl"); // Add the special class for Ana Dunovic
-        usernameSpan.style.color = "darkblue"; // Mordor blue color code
-        usernameSpan.textContent = [OWNER'S Girl] ${username}: ; // Prepend the prefix
+        usernameSpan.textContent = `[OWNER'S Girl] ${username}: `; // Prepend the prefix
       } else {
-        usernameSpan.textContent = ${username}: ;
+        usernameSpan.textContent = `${username}: `;
       }
 
       const messageSpan = document.createElement("span");
@@ -130,4 +127,4 @@ document.getElementById("message").addEventListener("keypress", (e) => {
 });
 
 // Start listening to messages
-listenToMessages()
+listenToMessages();
