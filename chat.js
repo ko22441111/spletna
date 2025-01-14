78812de2b09dadfe2bc8f908e1cafaddac8968be
ko22441111 <dunovicana@gmail.com>
2025-01-14 10:44:42 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-SF7C1QWD83"
 };
 
-// Inicializacija Firebase
+// Inicializacija Firebase.
 let app, db;
 try {
   app = initializeApp(firebaseConfig);
